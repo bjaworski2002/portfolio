44e1c77324cbf47a9b166a8721af539f0dafa8f2
bjaworski2002 <bjaworski2002@gmail.com>
2021-06-20 12:01:28 +0200

    header

---
+++ src/header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Dropdown from "./Dropdown"
+import styles from "./Header.module.css"
+
+function Header(){
+    return(
+        <React.Fragment>
+            <header className="absolute left-0 top-0 h-auto w-full bg-black text-white flex justify-between flex-wrap">
+                <div className="p-2 px-4 text-lg font-bold">Bartosz Jaworski</div>
+                <Dropdown />
+            </header>
+        </React.Fragment>
+    )
+}
+export default Header
