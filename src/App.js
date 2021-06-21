aecbb790c51807a5717b2de79d5f4659559294ca
bjaworski2002 <bjaworski2002@gmail.com>
2021-06-21 12:39:07 +0200

    dodany modul "projects"

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from "react";
 import Header from "./header/Header"
 import Body from "./body/Body";
+import Projects from "./projects/Projects";
 function App() {
   return (
   <React.Fragment>
       <Header />
       <Body />
+      <Projects />
   </React.Fragment>
   );
 }
