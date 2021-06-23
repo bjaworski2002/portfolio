0e6e676b3fa6cc7e5d8de5570963ba51680b1428
bjaworski2002 <bjaworski2002@gmail.com>
2021-06-23 15:02:36 +0200

    Title added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import React from "react";
 import Header from "./header/Header"
+import { Helmet } from 'react-helmet'
 import Body from "./body/Body";
 import Projects from "./projects/Projects";
 function App() {
+
   return (
   <React.Fragment>
+      <Helmet>
+          <title>{ "Portfolio - Bartosz Jaworski" }</title>
+      </Helmet>
       <Header />
       <Body />
       <Projects />
